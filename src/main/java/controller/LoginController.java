2f427fb785d54e8bc5068f45cc5e2ac0687ffd18
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-07-13 15:13:38 +0530

    controllers updated

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    private TextField txtname;
+    static String username;
+
+    @FXML
+    void NameOnAction(ActionEvent event) throws IOException {
+        username=txtname.getText();
+        txtname.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LoginController.class.getResource("/view/ClientForm.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+}
